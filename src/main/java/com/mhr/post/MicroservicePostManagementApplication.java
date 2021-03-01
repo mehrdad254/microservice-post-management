daf90adbd8ab54102691dd3d24e08bf5bb912b47
Mehrdad Asalian <lost.mhr69@gmail.com>
2021-03-01 15:43:12 +0330

    finish

---
+++ src/main/java/com/mhr/post/MicroservicePostManagementApplication.java
@@ -0,0 +1,13 @@
+package com.mhr.post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicePostManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicePostManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mhr/post/MicroservicePostManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhr.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePostManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
